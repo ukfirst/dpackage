a26dce31a2b2f24efe7c85ff0bb799f73af6d5c8
ukfirst <runeterkin@googlemail.com>
2024-02-29 16:36:20 +0300

    Create repo

--- charts/__init__.py
+++ charts/__init__.py
@@ -1,18 +1,48 @@
 __version__ = '0.1'
+
 import pandas as pd
 import seaborn as sns
 import numpy as np
 import matplotlib.pyplot as plt
-def best_correlation(df, isrc, show_heatmap=False, amount=3, isrc_column='ISRC', date_column='DATE', total_column='TOTAL'):
+from dataclasses import dataclass
+
+
+@dataclass
+class MusicAnalysis:
+    """Class to analyze and return different analytical insights about song data"""
+    df: pd.DataFrame
+    isrc_column: str = 'ISRC'
+    date_column: str = 'DATE'
+    total_column: str = 'TOTAL'
+
+    def best_correlation(self, isrc, amount, show_heatmap=False):
+        """Find the best correlated tracks by their performance"""
+        df = pd.pivot_table(self.df, values=self.total_column, index=[self.date_column], columns=self.isrc_column, aggfunc=np.sum)
+        if show_heatmap:
+            plt.figure(figsize=(15, 10))
+            sns.heatmap(df.corr(), cmap='RdBu', annot=True,
+                        annot_kws={"size": 7}, vmin=-1, vmax=1)
+        df = df.corr().unstack().sort_values(ascending=False, key=abs).drop_duplicates()
+        df = pd.DataFrame(df)
+        df.index.rename(['ISRC1', 'ISRC2'], inplace=True)
+        df = df.query(f"ISRC1=='{isrc}' & 0!=1").head(amount)
+        df.reset_index(inplace=True)
+        df = df['ISRC2'].tolist()
+        df.append(isrc)
+        return df
+
+
+def best_correlation(df, isrc, show_heatmap=False, amount=3, isrc_column='ISRC', date_column='DATE',
+                     total_column='TOTAL'):
     """Find the best correlated tracks by their performance"""
-    df = pd.pivot_table(df, values=total_column, index=[date_column],columns=isrc_column, aggfunc=np.sum)
+    df = pd.pivot_table(df, values=total_column, index=[date_column], columns=isrc_column, aggfunc=np.sum)
     if show_heatmap:
-        plt.figure(figsize=(15,10))
+        plt.figure(figsize=(15, 10))
         sns.heatmap(df.corr(), cmap='RdBu', annot=True,
-            annot_kws={"size": 7}, vmin=-1, vmax=1)
+                    annot_kws={"size": 7}, vmin=-1, vmax=1)
     df = df.corr().unstack().sort_values(ascending=False, key=abs).drop_duplicates()
     df = pd.DataFrame(df)
-    df.index.rename(['ISRC1','ISRC2'], inplace=True)
+    df.index.rename(['ISRC1', 'ISRC2'], inplace=True)
     df = df.query(f"ISRC1=='{isrc}' & 0!=1").head(amount)
     df.reset_index(inplace=True)
     df = df['ISRC2'].tolist()
